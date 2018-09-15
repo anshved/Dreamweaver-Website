5af58142e75f2157b8c7d4b0b483a8296b3cd4fd
Vivek Gawande <vivek040997@gmail.com>
2018-09-15 16:04:54 +0530

    Add Database

--- src/includes/connect.inc.php
+++ src/includes/connect.inc.php
@@ -1,6 +1,6 @@
 <?php
-$dbServername= "localhost";
-$dbUsername= "root";
-$dbPassword= "";
-$dbName= "dreamweaverDb";
+$dbServername= "sql12.freemysqlhosting.net:3306";
+$dbUsername= "sql12256885";
+$dbPassword= "6yFQEyBSVv";
+$dbName= "sql12256885";
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName) or die("Connection failed");
