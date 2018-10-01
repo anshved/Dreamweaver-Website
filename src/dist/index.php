dbd83a7827f23d269237e093d870f534a497b635
Vivek Gawande <vivek040997@gmail.com>
2018-10-01 19:44:05 +0530

    Add logo in admin panel

--- src/dist/index.php
+++ src/dist/index.php
@@ -62,9 +62,9 @@
     <button class="navbar-toggler sidebar-toggler d-lg-none mr-auto" type="button" data-toggle="sidebar-show">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <a class="navbar-brand" href="#">
-      <img class="navbar-brand-full" src="img/brand/logo.svg" width="89" height="25" alt="CoreUI Logo">
-      <img class="navbar-brand-minimized" src="img/brand/sygnet.svg" width="30" height="30" alt="CoreUI Logo">
+    <a class="navbar-brand" href="/home.html">
+      <img class="navbar-brand-full" src="../img/dreamweaver.png" width="200" height="100" alt="Dreamweaver Logo">
+      <img class="navbar-brand-minimized" src="../img/dreamweaver.png" width="200" height="100" alt="Dreamweaver Logo">
     </a>
     <button class="navbar-toggler sidebar-toggler d-md-down-none" type="button" data-toggle="sidebar-lg-show">
       <span class="navbar-toggler-icon"></span>
