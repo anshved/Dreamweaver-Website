647c280b1947a2819c87893fde38c420f7c19e7a
Vivek Gawande <vivek040997@gmail.com>
2018-10-09 17:22:05 +0530

    Reduce database size

--- src/dreamweaverDb.sql
+++ src/dreamweaverDb.sql
@@ -10,12 +10,12 @@ SET NAMES utf8mb4;
 DROP TABLE IF EXISTS `albums`;
 CREATE TABLE `albums` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `singers` varchar(512) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` varchar(512) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `date` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `banner` varchar(512) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `status` varchar(512) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `singers` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `banner` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
   `date_created` date NOT NULL,
   PRIMARY KEY (`id`),
   KEY `status` (`status`)
@@ -25,10 +25,10 @@ CREATE TABLE `albums` (
 DROP TABLE IF EXISTS `animation`;
 CREATE TABLE `animation` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` varchar(512) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `banner` varchar(512) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `status` varchar(512) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `banner` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
   `date_created` date NOT NULL,
   PRIMARY KEY (`id`),
   KEY `status` (`status`)
@@ -38,14 +38,14 @@ CREATE TABLE `animation` (
 DROP TABLE IF EXISTS `dreamer_upload`;
 CREATE TABLE `dreamer_upload` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `first` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `last` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `type` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `first` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
   `resume` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
   `contact` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
   `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `filename` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `filename` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -53,13 +53,13 @@ CREATE TABLE `dreamer_upload` (
 DROP TABLE IF EXISTS `movies`;
 CREATE TABLE `movies` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `actors` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `date` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` varchar(512) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `duration` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `banner` varchar(512) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `status` varchar(512) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `actors` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `duration` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `banner` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
   `date_created` date NOT NULL,
   PRIMARY KEY (`id`),
   KEY `status` (`status`)
@@ -69,9 +69,9 @@ CREATE TABLE `movies` (
 DROP TABLE IF EXISTS `news`;
 CREATE TABLE `news` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `headline` varchar(512) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` varchar(512) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `image` varchar(512) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `headline` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -79,7 +79,7 @@ CREATE TABLE `news` (
 DROP TABLE IF EXISTS `slides`;
 CREATE TABLE `slides` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `image_name` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image_name` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -88,7 +88,7 @@ DROP TABLE IF EXISTS `trailers`;
 CREATE TABLE `trailers` (
   `trailer_id` int(11) NOT NULL AUTO_INCREMENT,
   `movie_id` int(11) NOT NULL,
-  `trailer_name` varchar(512) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `trailer_name` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`trailer_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -96,8 +96,8 @@ CREATE TABLE `trailers` (
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_email` varchar(512) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `user_pass` varchar(512) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_pass` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `u_email` (`user_email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -106,13 +106,13 @@ CREATE TABLE `users` (
 DROP TABLE IF EXISTS `webseries`;
 CREATE TABLE `webseries` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `actors` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` varchar(512) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `actors` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
   `season` int(4) NOT NULL,
-  `date` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `banner` varchar(512) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `status` varchar(512) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `banner` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
   `date_created` date NOT NULL,
   PRIMARY KEY (`id`),
   KEY `status` (`status`)
@@ -123,7 +123,7 @@ DROP TABLE IF EXISTS `webseries_trailer`;
 CREATE TABLE `webseries_trailer` (
   `wb_trailer_id` int(11) NOT NULL AUTO_INCREMENT,
   `webseries_id` int(11) NOT NULL,
-  `wb_trailer_name` varchar(512) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `wb_trailer_name` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`wb_trailer_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
